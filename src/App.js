08bcb8da15e6f6beec1a0a7e700bdba2c5bba754
Jonathan Jacka <jonathanjacka@gmail.com>
2022-08-03 21:59:15 -0500

    Added routes and navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import NavBar from './components/NavBar';
+
+import Explore from './views/Explore';
+import Profile from './views/Profile';
+import Offers from './views/Offers';
+import SignUp from './views/SignUp';
+import SignIn from './views/SignIn';
+import ForgotPassword from './views/ForgotPassword';
+
 function App() {
   return (
-    <div>
-      <h1>My App</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/profile' element={<SignIn />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+        <NavBar />
+      </Router>
+    </>
   );
 }
 
