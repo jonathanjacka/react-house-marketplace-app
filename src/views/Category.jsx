20b83c000a6062b7fdccb96a2cda7257839ebea3
Jonathan Jacka <jonathanjacka@gmail.com>
2022-08-30 07:09:03 -0500

    Added pagination to category and offers page

--- src/views/Category.jsx
+++ src/views/Category.jsx
@@ -12,8 +12,39 @@ function Category() {
 
     const [ listings, setLisitings ] = useState(null);
     const [ loading, setLoading ] = useState(true);
+    const [ lastFetchedListing , setLastFetchedListing ] = useState(null);
 
     const {categoryName} = useParams();
+
+    const handlePagination = async () => {
+        try {
+            //get collections ref from fb
+            const listingsRef = collection(db, 'listings');
+            //set query in fb
+            const qry = query(
+                listingsRef, 
+                where('type', '==', categoryName), 
+                orderBy('timestamp', 'desc'), 
+                startAfter(lastFetchedListing),
+                limit(10));
+            //exe query in fb
+            const qrySnapShot = await getDocs(qry);
+            //set page
+            const lastVisible = qrySnapShot.docs[qrySnapShot.docs.length - 1];
+            setLastFetchedListing(lastVisible);
+            !lastVisible && toast.info('There are no more listings');
+            //get data from completed query
+            const listings = [];
+            qrySnapShot.forEach(doc => listings.push({ id: doc.id, data: doc.data()}));
+
+            //set data to local state
+            setLisitings(prevState => [...prevState, ...listings]);
+            setLoading(false);
+
+        } catch (error) {
+            toast.error('Unable to fetch listings');
+        }
+    }
 
     useEffect( () => {
         const fetchLisitings = async () => {
@@ -21,9 +52,12 @@ function Category() {
                 //get collections ref from fb
                 const listingsRef = collection(db, 'listings');
                 //set query in fb
-                const qry = query(listingsRef, where('type', '==', categoryName), orderBy('timestamp', 'desc', limit(10)));
+                const qry = query(listingsRef, where('type', '==', categoryName), orderBy('timestamp', 'desc'), limit(10));
                 //exe query in fb
                 const qrySnapShot = await getDocs(qry);
+                //set page
+                const lastVisible = qrySnapShot.docs[qrySnapShot.docs.length - 1];
+                setLastFetchedListing(lastVisible);
                 //get data from completed query
                 const listings = [];
                 qrySnapShot.forEach(doc => listings.push({ id: doc.id, data: doc.data()}));
@@ -33,7 +67,7 @@ function Category() {
                 setLoading(false);
 
             } catch (error) {
-                toast.error('Unable to fetch listings: ' + error.message);
+                toast.error('Unable to fetch listings');
             }
         }
         fetchLisitings();
@@ -54,6 +88,13 @@ function Category() {
                             <ListingItem key={listing.id} listing={listing.data} id={listing.id}/>
                         )}
                     </ul>
+
+                    <br />
+                    <br />
+
+                    {lastFetchedListing && (
+                        <p className='loadMore' onClick={handlePagination}>Load More</p>
+                    )}
                 </main>
             </>
          : <p>{`Currently, there are no listings for ${categoryName}.`}</p>}
